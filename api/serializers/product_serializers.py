96bd9d3e0a1ae1f20444925342cc342b4ff28949
kuba <kuba.baginski1507@gmail.com>
2023-10-08 17:15:36 +0200

    product api skeleton

---
+++ api/serializers/product_serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.Serializer):
+    pass
+
+
+class ProductSerializer(serializers.Serializer):
+    pass
+
+
+class ProductBookmarkSerializer(serializers.Serializer):
+    pass
+
+
+class OrderDetailSerializer(serializers.Serializer):
+    pass
+
+
+class OrderSerializer(serializers.Serializer):
+    pass
