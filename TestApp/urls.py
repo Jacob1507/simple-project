1ea1b60d51dfd8d1701ae93cd0107feb541d8cc6
Kuba <jakub.baginski@grywit.pl>
2023-11-03 22:38:50 +0100

    temp changes 2

--- TestApp/urls.py
+++ TestApp/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from products import views
@@ -23,4 +25,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("", views.product_list, name="products_list"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
